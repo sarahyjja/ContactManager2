1ce91e17612aedbea0f701bf8ceb4f650b72f172
sarah <sarah@Beccas-MBP-2.localdomain>
2018-11-15 18:20:38 +0000

    End of the day. Working on last test in contact_list.rb

--- lib/contact_list.rb
+++ lib/contact_list.rb
@@ -4,13 +4,21 @@ class ContactList
     @list = []
   end
 
-  def add_new_contact(name)
-    @list << name
+  def add_new_contact(person)
+    @list << person
   end
 
   def list
-    @list.sort
+    @list.sort_by do |person|
+      person.last_name
+    end
+    # do |person|
+    #   person.first_name
+    # end
     #_by { |key, value| value }
   end
 
+def search(last_name)
+  @list
+end
 end
--- spec/contact_list_spec.rb
+++ spec/contact_list_spec.rb
@@ -2,6 +2,7 @@
 # alphabetic order
 
 require 'contact_list'
+require 'person'
 
 RSpec.describe ContactList do
   it "Verify if the contact_list is empty" do
@@ -11,36 +12,36 @@ RSpec.describe ContactList do
 
   it "hold info inside" do
     contact_list = ContactList.new
-    contact_list.add_new_contact("sarah")
-    expect(contact_list.list).to eq(["sarah"])
+    person = Person.new("Sarah", "Kharraz", "sarah@example.com", 012344556)
+    contact_list.add_new_contact(person)
+    expect(contact_list.list).to eq([person])
   end
 
-  # it "displays a list in alphabetical order by default" do
-  #   contact_list = ContactList.new(:last_name)
-  #
-  #   sarah = Person.new("Sarah", "Kharraz", "sarah@example.com", 012344556)
-  #   john = Person.new("John", "Smith", "john@example.com", 012344556)
-  #   marion = Person.new("Marion", "Faceless", "marion@example.com", 012344556)
-  # #
-  # #   contact_list.add_new_contact(marion)
-  # #   contact_list.add_new_contact(sarah)
-  # #   contact_list.add_new_contact(john)
-  # #
-  #   expect(contact_list.list).to eq([{
-  #       "first_name" => "Sarah",
-  #       "last_name" => "Kharraz",
-  #       "email_address" => "sarah@email.com",
-  #       "phone_number" => "123456"
-  #     }])
-  #end
-
-  it "display a list in alphabetical order by default" do
+  it "displays a list in alphabetical order by default" do
     contact_list = ContactList.new
 
-    contact_list.add_new_contact("marion")
-    contact_list.add_new_contact("sarah")
-    contact_list.add_new_contact("john")
+    sarah = Person.new("Sarah", "Kharraz", "sarah@example.com", 012344556)
+    john = Person.new("John", "Smith", "john@example.com", 012344556)
+    marion = Person.new("Marion", "Faceless", "marion@example.com", 012344556)
 
-    expect(contact_list.list).to eq(["john", "marion", "sarah"])
+    contact_list.add_new_contact(marion)
+    contact_list.add_new_contact(john)
+    contact_list.add_new_contact(sarah)
+
+    expect(contact_list.list).to eq([marion, sarah, john])
+  end
+
+  it "search for a contact by last_name and view their details" do
+    contact_list = ContactList.new
+    sarah = Person.new("Sarah", "Kharraz", "sarah@example.com", 012344556)
+    john = Person.new("John", "Smith", "john@example.com", 012344556)
+    marion = Person.new("Marion", "Faceless", "marion@example.com", 012344556)
+
+    contact_list.add_new_contact(marion)
+    contact_list.add_new_contact(john)
+    contact_list.add_new_contact(sarah)
+
+    expect(contact_list.search("sarah")).to eq(sarah)
+
   end
 end
